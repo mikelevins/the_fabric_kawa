192f55ed3194f7ee5039f10cb0f736075e9d773d
mikel evins <mevins@me.com>
2014-06-30 18:33:41 -0500

    changed camera to drag-to-rotate; added more scene-setup code

--- src2/application-workshop.scm
+++ src2/application-workshop.scm
@@ -27,6 +27,8 @@
 (define-private-alias AssetManager com.jme3.asset.AssetManager)
 (define-private-alias BloomFilter com.jme3.post.filters.BloomFilter)
 (define-private-alias FilterPostProcessor com.jme3.post.FilterPostProcessor)
+(define-private-alias Geometry com.jme3.scene.Geometry)
+(define-private-alias Mouse org.lwjgl.input.Mouse)
 (define-private-alias SkyFactory com.jme3.util.SkyFactory)
 
 
@@ -49,21 +51,49 @@
 ;;; accessor functions
 ;;; ---------------------------------------------------------------------
 
+;;; getters
+;;; ---------------------------------------------------------------------
+
+;;; app-settings:
 (defmethod appstate ((app FabricWorkshop) (key (singleton app-settings:)))
   (*:getAppSettings app))
 
+;;; root-node:
 (defmethod appstate ((app FabricWorkshop) (key (singleton root-node:)))
   (*:getRootNode app))
 
+;;; application-init:
 (defmethod appstate ((app FabricWorkshop) (key (singleton application-init:)))
   (get-key (*:getFrameState app)
            application-init: default-init-application))
 
+;;; viewport:
 (defmethod appstate ((app FabricWorkshop) (key (singleton viewport:)))
   (*:getViewPort app))
 
+;;; sky:
+(defmethod appstate ((app FabricWorkshop) (key (singleton sky:)))
+  (get-key (*:getFrameState app)
+           sky: #f))
+
+;;; fly-by-camera:
+(defmethod appstate ((app FabricWorkshop) (key (singleton fly-by-camera:)))
+  (*:getFlyByCamera app))
+
+;;; setters
+;;; ---------------------------------------------------------------------
+
+;;; application-init:
 (defmethod set-appstate! ((app FabricWorkshop) (key (singleton application-init:)) (initfn gnu.expr.ModuleMethod))
-  (set-key! (*:getFrameState app) application-init: initfn))
+  (set-key! (*:getFrameState app) application-init: initfn)
+  app)
+
+;;; sky:
+(defmethod set-appstate! ((app FabricWorkshop) (key (singleton sky:)) (sky Geometry))
+  (set-key! (*:getFrameState app) sky: sky)
+  (let ((root (appstate app root-node:)))
+    (*:attachChild root sky))
+  app)
 
 ;;; ---------------------------------------------------------------------
 ;;; application initialization
@@ -91,10 +121,13 @@
 
 (define (init-workshop app)
   (let ((default-sky (make-workshop-sky app))
-        (root (appstate app root-node:)))
+        (fly-cam (appstate app fly-by-camera:)))
     (setup-lighting app)
-    (*:attachChild root default-sky)
-    ))
+    (set-appstate! app sky: default-sky)
+    ;; don't seize the mouse from the user
+    (Mouse:setGrabbed #f)
+    (*:setDragToRotate fly-cam #t)
+    (*:setMoveSpeed fly-cam 100)))
 
 (define (make-workshop)
   (let ((shop (FabricWorkshop)))
