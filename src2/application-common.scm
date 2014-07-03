3437c51c65056e89b10d236b91499eff78902ba7
mikel evins <mevins@me.com>
2014-07-02 19:23:50 -0500

    cleaned up a few java warnings

--- src2/application-common.scm
+++ src2/application-common.scm
@@ -25,10 +25,12 @@
 (define-private-alias Box com.jme3.scene.shape.Box)
 (define-private-alias ColorRGBA com.jme3.math.ColorRGBA)
 (define-private-alias Geometry com.jme3.scene.Geometry)
+(define-private-alias HashPMap org.pcollections.HashPMap)
 (define-private-alias HashTreePMap org.pcollections.HashTreePMap)
 (define-private-alias LList gnu.lists.LList)
 (define-private-alias Map java.util.Map)
 (define-private-alias Material com.jme3.material.Material)
+(define-private-alias Node com.jme3.scene.Node)
 (define-private-alias SimpleApplication com.jme3.app.SimpleApplication)
 
 ;;; ---------------------------------------------------------------------
@@ -40,7 +42,7 @@
          (box (Box 1 1 1))
          (geom (Geometry "Box" box))
          (mat (Material asset-manager "Common/MatDefs/Misc/Unshaded.j3md"))
-         (root (get-key app root-node:)))
+         (root::Node (get-key app root-node:)))
     (*:setColor mat "Color" ColorRGBA:Blue)
     (*:setMaterial geom mat)
     (*:attachChild root geom)
@@ -54,7 +56,7 @@
   ;; slots
   ;; -------
   (app-settings init-form: (AppSettings #t))
-  (slots init-form: (hashpmap application-init: default-init-application))
+  (slots::HashPMap init-form: (hashpmap application-init: default-init-application))
   
   ;; accessors
   ;; ---------
