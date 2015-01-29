976f3b5b144ac6e22ac7c5c0d17790a0fb863f5c
mikel evins <mevins@me.com>
2015-01-29 05:58:45 -0600

    invented a scheme for reckoning time in the Fabric

--- etc/users.scm
+++ etc/users.scm
@@ -2,11 +2,11 @@
 ;;; salt = (compute-random-salt)
 ;;; password = (text->digest <password> <salt>)
 
-(username: "mikel"
-           password: "iulQwBjPrFaPyHM+5349TaGD3qg=" salt: (-45 -104 13 5 -83 -75 45 -80)
-           name: "mikel evins" roles: ("creator" "developer" "admin" "player"))
+(user username: "mikel" password: "iulQwBjPrFaPyHM+5349TaGD3qg="
+      salt: (-45 -104 13 5 -83 -75 45 -80) name: "mikel evins"
+      roles: ("creator" "developer" "admin" "player"))
 
-(username: "corey"
-           password: "zE8bsOPHoYVT56PuUlxQ7xPuW5g=" salt: (64 120 -40 -2 -117 70 -126 122)
-           name: "Corey Gagnon" roles: ("developer" "admin" "player"))
+(user username: "corey" password: "zE8bsOPHoYVT56PuUlxQ7xPuW5g="
+      salt: (64 120 -40 -2 -117 70 -126 122) name: "Corey Gagnon"
+      roles: ("developer" "admin" "player"))
 
