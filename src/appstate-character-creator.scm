f395b9dc21dd4a359118efac263bfa4937e4110a
mikel evins <mevins@me.com>
2015-02-23 01:14:27 -0600

    added butons for power and energy armor

--- src/appstate-character-creator.scm
+++ src/appstate-character-creator.scm
@@ -564,6 +564,72 @@
                (compute-regen-armor-button-origin screen)
                (compute-regen-armor-button-size screen)))
 
+
+;;; (compute-power-armor-button-origin screen::Screen)
+;;; ---------------------------------------------------------------------
+;;; computes and returns a suitable origin for the absorb armor
+;;; button
+
+
+(define (compute-power-armor-button-origin screen::Screen)
+  (let ((armor-palette-size (compute-armor-palette-size screen))
+        (armor-button-size (compute-power-armor-button-size screen)))
+    (Vector2f (- (/ (*:getX armor-palette-size) 2.0)
+                 (/ (*:getX armor-button-size) 2.0))
+              384)))
+
+
+;;; (compute-power-armor-button-size screen::Screen)
+;;; ---------------------------------------------------------------------
+;;; computes and returns a suitable size for the absorb armor
+;;; button
+
+
+(define (compute-power-armor-button-size screen::Screen)
+  (Vector2f 128 96))
+
+;;; (make-power-armor-button screen::Screen)
+;;; ---------------------------------------------------------------------
+;;; returns a newly-constructed absorb armor button
+
+(define (make-power-armor-button screen::Screen)
+  (RadioButton screen "PowerArmorButton"
+               (compute-power-armor-button-origin screen)
+               (compute-power-armor-button-size screen)))
+
+
+;;; (compute-energy-armor-button-origin screen::Screen)
+;;; ---------------------------------------------------------------------
+;;; computes and returns a suitable origin for the absorb armor
+;;; button
+
+
+(define (compute-energy-armor-button-origin screen::Screen)
+  (let ((armor-palette-size (compute-armor-palette-size screen))
+        (armor-button-size (compute-energy-armor-button-size screen)))
+    (Vector2f (- (/ (*:getX armor-palette-size) 2.0)
+                 (/ (*:getX armor-button-size) 2.0))
+              536)))
+
+
+;;; (compute-energy-armor-button-size screen::Screen)
+;;; ---------------------------------------------------------------------
+;;; computes and returns a suitable size for the absorb armor
+;;; button
+
+
+(define (compute-energy-armor-button-size screen::Screen)
+  (Vector2f 128 96))
+
+;;; (make-energy-armor-button screen::Screen)
+;;; ---------------------------------------------------------------------
+;;; returns a newly-constructed absorb armor button
+
+(define (make-energy-armor-button screen::Screen)
+  (RadioButton screen "EnergyArmorButton"
+               (compute-energy-armor-button-origin screen)
+               (compute-energy-armor-button-size screen)))
+
 ;;; ---------------------------------------------------------------------
 ;;; the augment palette
 ;;; ---------------------------------------------------------------------
@@ -688,6 +754,8 @@
            (armor-group (ArmorButtonGroup screen "ArmorGroup"))
            (absorb-armor-button (make-absorb-armor-button screen))
            (regen-armor-button (make-regen-armor-button screen))
+           (power-armor-button (make-power-armor-button screen))
+           (energy-armor-button (make-energy-armor-button screen))
            (weapons-palette (make-weapons-palette screen))
            (augment-palette (make-augment-palette screen)))
       ;; --------------------
@@ -772,7 +840,7 @@
       (*:addChild armor-palette absorb-armor-button)
       ;; regen armor button
       (*:setButtonIcon regen-armor-button 128 128 "Interface/regen-armor-icon128.png")
-      (*:setButtonPressedInfo regen-armor-button "Interface/absorb-armor-icon128.png"
+      (*:setButtonPressedInfo regen-armor-button "Interface/regen-armor-icon128.png"
                               (ColorRGBA 0.0 1.0 0.0 1.0))
       (*:setText regen-armor-button "Regenerate")
       (*:setTextAlign regen-armor-button align:Center)
@@ -780,6 +848,26 @@
       (*:setFontSize regen-armor-button 20)
       (*:addButton armor-group regen-armor-button)
       (*:addChild armor-palette regen-armor-button)
+      ;; power armor button
+      (*:setButtonIcon power-armor-button 128 128 "Interface/power-armor-icon128.png")
+      (*:setButtonPressedInfo power-armor-button "Interface/power-armor-icon128.png"
+                              (ColorRGBA 1.0 0.5 0.0 1.0))
+      (*:setText power-armor-button "Power")
+      (*:setTextAlign power-armor-button align:Center)
+      (*:setTextVAlign power-armor-button valign:Bottom)
+      (*:setFontSize power-armor-button 20)
+      (*:addButton armor-group power-armor-button)
+      (*:addChild armor-palette power-armor-button)
+      ;; energy armor button
+      (*:setButtonIcon energy-armor-button 128 128 "Interface/energy-armor-icon128.png")
+      (*:setButtonPressedInfo energy-armor-button "Interface/energy-armor-icon128.png"
+                              (ColorRGBA 0.0 6.0 1.0 1.0))
+      (*:setText energy-armor-button "Energy")
+      (*:setTextAlign energy-armor-button align:Center)
+      (*:setTextVAlign energy-armor-button valign:Bottom)
+      (*:setFontSize energy-armor-button 20)
+      (*:addButton armor-group energy-armor-button)
+      (*:addChild armor-palette energy-armor-button)
       ;; add palette to screen
       (*:addElement screen armor-palette)
       ;; --------------------
