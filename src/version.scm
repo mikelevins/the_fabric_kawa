1e50db41820593a304c4337e0f27b691c7e234c4
mikel evins <mevins@me.com>
2015-03-07 20:51:01 -0600

    bumped version; see notes
    Notes:
    - fixed a crash in state-switching caused by tonegod's undocumented requirement that all UI elements created must have session-unique IDs.
    - also, JME3's initialize and cleanup methods appear not to be called, in contradiction to documentation, which means that setup and teardown must be handled in stateAttached and stateDetached, which further means that they must be enqueued in order to avoid opengl crashes because of alterations to the scene graph on the wrong thread

--- src/version.scm
+++ src/version.scm
@@ -13,6 +13,6 @@
 ;;; fabric-version
 ;;; ---------------------------------------------------------------------
 ;;; the version of the Fabric project as a whole
-(define fabric-version (make-parameter "0.1.1d224"))
+(define fabric-version (make-parameter "0.1.1d235"))
 
 
