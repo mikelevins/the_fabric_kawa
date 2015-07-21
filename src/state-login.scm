6ebeb654f0656eb57cdfb7f8b68240922621d245
mikel evins <mevins@me.com>
2015-07-21 13:09:48 -0500

    working on getting loginstate up onscreen

--- src/state-login.scm
+++ src/state-login.scm
@@ -22,6 +22,8 @@
 (require "util-lists.scm")
 (require "data-nodes.scm")
 (require "client-state.scm")
+(require "client-class.scm")
+(require "view-loginbox.scm")
 
 ;;; ---------------------------------------------------------------------
 ;;; Java imports
@@ -29,6 +31,10 @@
 
 (import-as Application com.jme3.app.Application)
 (import-as AppStateManager com.jme3.app.state.AppStateManager)
+(import-as Node com.jme3.scene.Node)
+(import-as Screen tonegod.gui.core.Screen)
+(import-as Spatial com.jme3.scene.Spatial)
+(import-as Vector2f com.jme3.math.Vector2f)
 
 ;;; ---------------------------------------------------------------------
 ;;; the client login AppState class
@@ -76,3 +82,65 @@
   (let ((state (LoginState)))
     (*:setClient state client)
     state))
+
+;;; ---------------------------------------------------------------------
+;;; LoginState functions
+;;; ---------------------------------------------------------------------
+
+(define (compute-login-box-rect screen::Screen)
+  (let* ((screen-width (*:getWidth screen))
+         (screen-height (*:getHeight screen))
+         (box-width 700)
+         (box-height 300)
+         (box-left (- (/ screen-width 2)
+                      (/ box-width 2)))
+         (box-top (- (/ screen-height 2)
+                     (/ box-height 2))))
+    (make-rectangle box-left
+                    box-top
+                    box-width
+                    box-height)))
+
+(define (prepare-to-attach-login-client-state state::LoginState client::FabricClient)
+  (unless (*:getInitialized state)
+    (let* ((client::FabricClient (*:getApp state))
+           (screen::Screen (*:getScreen client))
+           (sky::Spatial (make-sky-box))
+           (gui-node::Node (*:getGuiNode client))
+           (rect (compute-login-box-rect screen))
+           (box::FabricLoginBox (FabricLoginBox screen "LoginBox"
+                                                (Vector2f (get-left rect) (get-top rect))
+                                                (Vector2f (get-width rect) (get-height rect)))))
+      (*:setWindowTitle box "Log in to the Fabric")
+      (*:setLoginBox state box)
+      (*:setSky state sky)
+      (*:setInitialized state #t))))
+
+(define (did-attach-login-client-state state::LoginState mgr::AppStateManager)
+  (when (*:getInitialized state)
+    (let* ((client::FabricClient (*:getApp state))
+           (screen::Screen (*:getScreen client))
+           (gui-node::Node (*:getGuiNode client)))
+      (*:enqueue client
+                 (runnable (lambda ()
+                             (let ((client::FabricClient (*:getApp state))
+                                   (root::Node (*:getRootNode client)))
+                               (*:attachChild root (*:getSky state))
+                               (*:addElement screen (*:getLoginBox state))
+                               (*:addControl gui-node screen))))))))
+
+(define (did-detach-login-client-state state::LoginState mgr::AppStateManager)
+  (when (*:getInitialized state)
+    (let* ((client::FabricClient (*:getApp state))
+           (screen::Screen (*:getScreen client))
+           (gui-node::Node (*:getGuiNode client)))
+      (*:enqueue client
+                 (runnable (lambda ()
+                             (let ((client::FabricClient (*:getApp state))
+                                   (root::Node (*:getRootNode client))
+                                   (sky::Spatial (*:getSky state)))
+                               (*:detachChild root sky)
+                               (*:setSky state #!null)
+                               (*:removeElement screen (*:getLoginBox state))
+                               (*:removeControl gui-node screen)
+                               (*:setInitialized state #f))))))))
