98194310bc128def5090ac43cb162e0e434f8ab1
mikel evins <mevins@me.com>
2015-08-07 04:01:44 -0500

    pick-character state enabled

--- src/state-pick-character.scm
+++ src/state-pick-character.scm
@@ -29,6 +29,13 @@
 
 (import-as Application com.jme3.app.Application)
 (import-as AppStateManager com.jme3.app.state.AppStateManager)
+(import-as BitmapFont com.jme3.font.BitmapFont)
+(import-as Label tonegod.gui.controls.text.Label)
+(import-as Node com.jme3.scene.Node)
+(import-as Screen tonegod.gui.core.Screen)
+(import-as Spatial com.jme3.scene.Spatial)
+(import-as Vector2f com.jme3.math.Vector2f)
+(import-as Window tonegod.gui.controls.windows.Window)
 
 ;;; ---------------------------------------------------------------------
 ;;; the client character-creator AppState class
@@ -73,3 +80,26 @@
   (let ((state (PickCharacterState)))
     (*:setClient state client)
     state))
+
+;;; ---------------------------------------------------------------------
+;;; PickCharacterState functions
+;;; ---------------------------------------------------------------------
+
+(define (prepare-to-attach-pick-character-state state::PickCharacterState client)
+  (unless (*:getInitialized state)
+          (let* ((screen::Screen (*:getScreen client))
+                 (gui-node::Node (*:getGuiNode client))
+                 (Align BitmapFont:Align)
+                 )
+            (*:setInitialized state #t))))
+
+(define (did-attach-pick-character-state state::PickCharacterState mgr::AppStateManager)
+  (when (*:getInitialized state)
+        (let* ((client (*:getClient state))
+               (screen::Screen (*:getScreen client))
+               (gui-node::Node (*:getGuiNode client)))
+          (*:enqueue client
+                     (runnable (lambda ()
+                                 (let ((client (*:getClient state))
+                                       (root::Node (*:getRootNode client)))
+                                   (*:addControl gui-node screen))))))))
