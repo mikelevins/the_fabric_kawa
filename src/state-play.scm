1244df62bc07e288290aa317106b0e105cc4396c
mikel evins <mevins@me.com>
2015-08-07 13:40:14 -0500

    play and transit states restored

--- src/state-play.scm
+++ src/state-play.scm
@@ -29,6 +29,13 @@
 
 (import-as Application com.jme3.app.Application)
 (import-as AppStateManager com.jme3.app.state.AppStateManager)
+(import-as BitmapFont com.jme3.font.BitmapFont)
+(import-as Label tonegod.gui.controls.text.Label)
+(import-as Node com.jme3.scene.Node)
+(import-as Screen tonegod.gui.core.Screen)
+(import-as Spatial com.jme3.scene.Spatial)
+(import-as Vector2f com.jme3.math.Vector2f)
+(import-as Window tonegod.gui.controls.windows.Window)
 
 ;;; ---------------------------------------------------------------------
 ;;; the client play AppState class
@@ -77,3 +84,26 @@
     (*:setClient state client)
     (*:setNodeName state node-name)
     state))
+
+;;; ---------------------------------------------------------------------
+;;; PlayState functions
+;;; ---------------------------------------------------------------------
+
+(define (prepare-to-attach-play-state state::PlayState client)
+  (unless (*:getInitialized state)
+          (let* ((screen::Screen (*:getScreen client))
+                 (gui-node::Node (*:getGuiNode client))
+                 (Align BitmapFont:Align)
+                 )
+            (*:setInitialized state #t))))
+
+(define (did-attach-play-state state::PlayState mgr::AppStateManager)
+  (when (*:getInitialized state)
+        (let* ((client (*:getClient state))
+               (screen::Screen (*:getScreen client))
+               (gui-node::Node (*:getGuiNode client)))
+          (*:enqueue client
+                     (runnable (lambda ()
+                                 (let ((client (*:getClient state))
+                                       (root::Node (*:getRootNode client)))
+                                   (*:addControl gui-node screen))))))))
--- src/state-transit.scm
+++ src/state-transit.scm
@@ -29,6 +29,13 @@
 
 (import-as Application com.jme3.app.Application)
 (import-as AppStateManager com.jme3.app.state.AppStateManager)
+(import-as BitmapFont com.jme3.font.BitmapFont)
+(import-as Label tonegod.gui.controls.text.Label)
+(import-as Node com.jme3.scene.Node)
+(import-as Screen tonegod.gui.core.Screen)
+(import-as Spatial com.jme3.scene.Spatial)
+(import-as Vector2f com.jme3.math.Vector2f)
+(import-as Window tonegod.gui.controls.windows.Window)
 
 ;;; ---------------------------------------------------------------------
 ;;; the client transit AppState class
@@ -81,3 +88,26 @@
     (*:setFromName state from)
     (*:setToName state to)
     state))
+
+;;; ---------------------------------------------------------------------
+;;; TransitState functions
+;;; ---------------------------------------------------------------------
+
+(define (prepare-to-attach-transit-state state::TransitState client)
+  (unless (*:getInitialized state)
+          (let* ((screen::Screen (*:getScreen client))
+                 (gui-node::Node (*:getGuiNode client))
+                 (Align BitmapFont:Align)
+                 )
+            (*:setInitialized state #t))))
+
+(define (did-attach-transit-state state::TransitState mgr::AppStateManager)
+  (when (*:getInitialized state)
+        (let* ((client (*:getClient state))
+               (screen::Screen (*:getScreen client))
+               (gui-node::Node (*:getGuiNode client)))
+          (*:enqueue client
+                     (runnable (lambda ()
+                                 (let ((client (*:getClient state))
+                                       (root::Node (*:getRootNode client)))
+                                   (*:addControl gui-node screen))))))))
