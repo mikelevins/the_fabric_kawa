47915e1bfc723d078d872e6f0fb47e9b535fc816
mikel evins <mevins@me.com>
2015-09-03 14:34:24 -0500

    added character-creator infrastructure

--- src/state-create-character.scm
+++ src/state-create-character.scm
@@ -11,6 +11,11 @@
 
 (module-export
  CreateCharacterState
+ compute-armor-picker-rect
+ compute-augment-picker-rect
+ compute-faction-picker-rect
+ compute-name-picker-rect
+ compute-weapon-picker-rect
  make-create-character-state)
 
 ;;; ---------------------------------------------------------------------
@@ -21,21 +26,41 @@
 (require "util-java.scm")
 (require "util-lists.scm")
 (require "data-nodes.scm")
+(require "model-rect.scm")
 (require "client-state.scm")
 (require "client-class.scm")
+(require "view-skybox.scm")
+(require "view-character-nameplate.scm")
+(require "view-faction-nameplate.scm")
+(require "view-character-acceptor.scm")
+(require "view-armor-picker.scm")
+(require "view-augment-picker.scm")
+(require "view-faction-picker.scm")
+(require "view-name-picker.scm")
+(require "view-weapon-picker.scm")
 
 ;;; ---------------------------------------------------------------------
 ;;; Java imports
 ;;; ---------------------------------------------------------------------
 
-(import-as Application com.jme3.app.Application)
+
+(import-as AbstractAppState com.jme3.app.state.AbstractAppState)
 (import-as AppStateManager com.jme3.app.state.AppStateManager)
+(import-as Application com.jme3.app.Application)
 (import-as BitmapFont com.jme3.font.BitmapFont)
+(import-as ButtonAdapter tonegod.gui.controls.buttons.ButtonAdapter)
+(import-as Client com.jme3.network.Client)
+(import-as ConnectException java.net.ConnectException)
+(import-as DefaultClient com.jme3.network.base.DefaultClient)
 (import-as Label tonegod.gui.controls.text.Label)
+(import-as Network com.jme3.network.Network)
 (import-as Node com.jme3.scene.Node)
 (import-as Screen tonegod.gui.core.Screen)
+(import-as Serializer com.jme3.network.serializing.Serializer)
+(import-as SimpleApplication com.jme3.app.SimpleApplication)
 (import-as Spatial com.jme3.scene.Spatial)
 (import-as Vector2f com.jme3.math.Vector2f)
+(import-as Vector4f com.jme3.math.Vector4f)
 (import-as Window tonegod.gui.controls.windows.Window)
 
 ;;; ---------------------------------------------------------------------
@@ -87,6 +112,65 @@
 ;;; ---------------------------------------------------------------------
 ;;; CreateCharacterState functions
 ;;; ---------------------------------------------------------------------
+
+;;; layout computations
+
+(define (compute-faction-picker-rect screen::Screen)
+  (make-rectangle 8 8 640 200))
+
+(define (compute-weapon-picker-rect screen::Screen)
+  (let* ((faction-picker-rect (compute-faction-picker-rect screen))
+         (weapon-picker-left (get-left faction-picker-rect))
+         (weapon-picker-top (+ (get-top faction-picker-rect)
+                               (get-height faction-picker-rect)
+                               8))
+         (weapon-picker-width 256)
+         (weapon-picker-height 720))
+    (make-rectangle weapon-picker-left
+                    weapon-picker-top
+                    weapon-picker-width
+                    weapon-picker-height)))
+
+(define (compute-armor-picker-rect screen::Screen)
+  (let* ((weapon-picker-rect (compute-weapon-picker-rect screen))
+         (screen-width (*:getWidth screen))
+         (armor-picker-width (get-width weapon-picker-rect))
+         (armor-picker-height (get-height weapon-picker-rect))
+         (armor-picker-left (- screen-width (+ armor-picker-width 8)))
+         (armor-picker-top (get-top weapon-picker-rect)))
+    (make-rectangle armor-picker-left
+                    armor-picker-top
+                    armor-picker-width
+                    armor-picker-height)))
+
+(define (compute-augment-picker-rect screen::Screen)
+  (let* ((faction-picker-rect (compute-faction-picker-rect screen))
+         (screen-width (*:getWidth screen))
+         (augment-picker-width (get-width faction-picker-rect))
+         (augment-picker-height (get-height faction-picker-rect))
+         (augment-picker-left (- screen-width augment-picker-width 8))
+         (augment-picker-top (get-top faction-picker-rect)))
+    (make-rectangle augment-picker-left
+                    augment-picker-top
+                    augment-picker-width
+                    augment-picker-height)))
+
+
+(define (compute-name-picker-rect screen::Screen)
+  (let* ((weapon-picker-rect (compute-weapon-picker-rect screen))
+         (screen-width (*:getWidth screen))
+         (screen-height (*:getHeight screen))
+         (picker-width (- screen-width (+ 24 (get-width weapon-picker-rect))))
+         (picker-height 220)
+         (picker-top (- screen-height
+                        (+ picker-height 8)))
+         (picker-left 8))
+    (make-rectangle picker-left
+                    picker-top
+                    picker-width
+                    picker-height)))
+
+;;; attach and detach
 
 (define (prepare-to-attach-create-character-state state::CreateCharacterState client::FabricClient)
   (unless (*:getInitialized state)
