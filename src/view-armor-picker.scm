9e8e1f01fa856c3babfdd63e770df18348126eec
mikel evins <mevins@me.com>
2015-09-23 23:27:40 -0500

    more rebuilding

--- src/view-armor-picker.scm
+++ src/view-armor-picker.scm
@@ -1,16 +1,16 @@
 ;;;; ***********************************************************************
 ;;;;
-;;;; Name:          view-faction-picker.scm
+;;;; Name:          view-armor-picker.scm
 ;;;; Project:       The Fabric: a far-future MMORPG
-;;;; Purpose:       the faction picker 
+;;;; Purpose:       the armor picker 
 ;;;; Author:        mikel evins
 ;;;; Copyright:     2014 by mikel evins
 ;;;;
 ;;;; ***********************************************************************
 
 (module-export
- compute-faction-picker-rect
- make-faction-picker)
+ compute-armor-picker-rect
+ make-armor-picker)
 
 ;;; ---------------------------------------------------------------------
 ;;; required modules
@@ -19,6 +19,7 @@
 (require state)
 (require state-create-character)
 (require model-rect)
+(require view-faction-picker)
 
 ;;; ---------------------------------------------------------------------
 ;;; Java imports
@@ -32,14 +33,21 @@
 ;;; 
 ;;; ---------------------------------------------------------------------
 
-(define (compute-faction-picker-rect screen::Screen)
-  (make-rectangle 8 8 640 200))
+(define (compute-armor-picker-rect screen::Screen)
+  (let* ((faction-rect (compute-faction-picker-rect screen))
+         (screen-width (*:getWidth screen))
+         (screen-height (*:getHeight screen))
+         (picker-left (get-left faction-rect))
+         (picker-top (+ 32 (get-top faction-rect)(get-height faction-rect)))
+         (picker-width 256)
+         (picker-height (- screen-height picker-top 32)))
+    (make-rectangle picker-left picker-top picker-width picker-height)))
 
-(define (make-faction-picker state::FabricClientState screen::Screen)
+(define (make-armor-picker state::FabricClientState screen::Screen)
   (let* ((state::CreateCharacterState (as CreateCharacterState state))
-         (rect (compute-faction-picker-rect screen))
-         (win (Window screen "FactionPicker"
+         (rect (compute-armor-picker-rect screen))
+         (win (Window screen "ArmorPicker"
                       (Vector2f (get-left rect) (get-top rect))
                       (Vector2f (get-width rect) (get-height rect)))))
-    (*:setWindowTitle win "Choose a faction:")
+    (*:setWindowTitle win "Choose your armor:")
     win))
